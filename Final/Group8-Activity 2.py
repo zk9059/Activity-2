e6eaa1ce6f038423a442bae8b8911f0c05c5dce8
zk9059 <zk9059@rit.edu>
2024-02-28 21:41:16 +0400

    Push 1

---
+++ Final/Group8-Activity 2.py
@@ -0,0 +1,207 @@
+#test for activity 2 
+'''
+Docsstrings
+
+Activity 2 done by 
+
+'''
+#listed here are all the conversion rates in variables
+
+
+
+
+Aed_to_Eruo=0.25
+Aed_to_British_pound= 0.22
+Aed_to_Dollars= 0.27
+
+
+Dollar_to_AED= 3.67
+British_Pound_to_Aed= 4.65
+Euro_to_AED= 3.98
+
+
+
+def Menu():
+    print("          Main Menu  ")
+    print("welcome to the Currency Converter ")
+    print('------------------------------------')
+    print("")
+
+
+def Conversion_direction_selection():
+    
+    print("select one of the following options:")
+    selection=int(input("Select the conversion direction: \n 1. AED to other currencies \n 2. Other currencies to AED \n 3. Exit \n"))
+    if selection ==1:
+        print("decision 1 ")
+        Money= Amount_to_be_Converted()
+        
+        AED_to_other_currencies(Money)
+
+    elif selection== 2:
+        print("decsion 2")  
+        Amount = Amount_to_be_Converted()
+        Other_currencies_to_Aed(Amount)
+
+    else:
+        print('exiting program' )
+        exit()    
+
+
+#THIS IS FOR OTHER CURRENCIES TO AED
+
+
+def Other_currencies_to_Aed(Amount):
+
+
+    
+    print('enter choices 1 2 or 3')
+    print('1. Euro (EUR) to AED')
+    print('2. British Pound (GBP) to AED')
+    print('3. Dollar to AED')
+    print('4. Exit')
+    Choice_Currency = int(input('enter subchoice here '))
+    if Choice_Currency ==1 :
+        eur_to_aed(Amount)
+    elif Choice_Currency == 2:
+        britishPound_to_aed(Amount)
+    elif Choice_Currency==3:
+        dollar_to_aed(Amount)
+    else:
+        exit()        
+
+
+
+
+def dollar_to_aed(Amount):
+    Converted_Result= Amount*Dollar_to_AED
+    print('the amount in aed is ' + str(Converted_Result))
+
+
+def britishPound_to_aed(Amount):
+    Converted_Result= Amount*British_Pound_to_Aed
+    print('the amount in AED is '+ str(Converted_Result))
+
+
+def eur_to_aed(Amount):
+    Converted_Result= Amount*Euro_to_AED
+    print(Amount +' in AED is '+ Converted_Result )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+def Amount_to_be_Converted():
+    print("input the amount of money you would like to convert")
+    Amount= float(input(""))
+    return Amount
+
+#the following code is for AED to other currencies 
+        
+
+def AED_to_other_currencies(Money ):
+   
+
+    #decide which currecny you want to take 
+    print('money ')
+    print("Enter Choice 1/2/3 ")
+    print("1. AED to Euro (EUR)")
+    print('2. AED to British Pound (GBP)')
+    print("3. AED to US Dollar")
+    print("4. AED to Exit")
+    Which_currency=int(input(''))
+
+    if Which_currency ==1:
+        aed_to_eur(Money)
+    elif Which_currency == 2:
+        aed_to_britishPound(Money)
+    elif Which_currency ==3:
+        aed_to_dollar(Money)    
+    else:
+        exit()    
+
+
+            
+
+
+
+
+
+
+def aed_to_britishPound(Money):
+    Converted_result= Money*Aed_to_British_pound
+    print(str(Money)+' in British Pounds'+ str(Converted_result))
+
+
+
+
+
+def aed_to_eur(Money):
+    print(Money)
+    Converted_Result=Money*Aed_to_Eruo
+    print(str(Money)+'in Euros is '+ str(Converted_Result))
+
+
+def aed_to_dollar(Money):
+    print("Aed to dollar")    
+    Converted_Result= Money*Aed_to_Dollars
+    print(str(Money)+" in dollars is "+ str(Converted_Result))
+
+
+
+
+
+
+
+
+
+def main():
+    Menu()
+    
+
+    Conversion_direction_selection()
+    #amount = Amount_to_be_Converted()
+    
+    
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+main()
+
+
+
+
+
+
+
+
+
--- Group-8-Activity2.py
+++ Group-8-Activity2.py
@@ -1,10 +1,74 @@
 '''
 this is an activity for GCIS 123 activity 2 by Zarak Khan , Khaled, Nour 
 
-
-
-
 '''
+
+decision=int(input("put in if you want 1 2 or 3 \n 1:do you want dhs to another currency \n 2: Do you want another currecny to dhs")) 
+decision2=int(input("input the amount you wish to convert"))
 
 
 #start out with the main introduction to the program 
+
+
+
+
+
+print("Hello welcome o the Currency Converter ")
+print('Here you can convert ')
+
+
+#step 1 user input, if they want xyz to dhs or they want dhs to xyz 
+ 
+
+def AskUserInputForCurrencyType():
+    decision=int(input("put in if you want 1 2 or 3 \n 1:do you want dhs to another currency \n 2: Do you want another currecny to dhs")) 
+    if decision ==1:
+        print("hello")
+        conversionXYZtoDHS()
+    
+
+    if decision == 2:
+        print("number2")  
+
+          
+
+
+
+
+def conversionXYZtoDHS():
+    print("hello")
+    decision3=int(input("choose one of the following \n 1: do you want  RMB to dhs\n 2: do you want rubles to dhs \n 3:euros to DHS "))
+    if decision3 ==2:
+        convertedRMBtoDHs=decision2/2.5
+        print(convertedRMBtoDHs)
+    elif decision3 == 2:
+        convertedRublestoDHS= decision2/5
+        print(convertedRublestoDHS)
+    elif decision3 == 3:
+        ConvertedEuroToDhs = decision3*6.2   
+    else:
+        print("input the values listed above ")
+
+
+    
+
+
+
+
+def askUserAmount():
+    print("hello")
+    
+    
+
+
+
+def main():
+    
+    
+    AskUserInputForCurrencyType()
+    
+    
+
+main()
+
+
